75c51c4207fe7183b944efff13f3fd165d029652
yamashita-takahiro-44 <shimolen@gmail.com>
2023-07-18 11:04:59 +0900

    fix_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
